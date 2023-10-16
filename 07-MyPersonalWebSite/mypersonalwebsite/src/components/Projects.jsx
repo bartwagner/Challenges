690b62883d90926d19d80ce28e12465373de542a
bartwagner <118787799+bartwagner@users.noreply.github.com>
2023-10-16 14:19:31 -0700

    new informations

--- 07-MyPersonalWebSite/mypersonalwebsite/src/components/Projects.jsx
+++ 07-MyPersonalWebSite/mypersonalwebsite/src/components/Projects.jsx
@@ -24,10 +24,10 @@ export default function Projects() {
                     <div className="project-inf">
                         <img src="./src/images/Itau.jpg" className="itau-img"/>
                         <div className="itau-inf">
-                            <p>In this project, I had the privilege of working both as a member of the development team and as a Scrum Master for a period of 5 months. It's worth noting that this website belongs to the largest bank in Latin America, Itaú.</p>
-                            <p>On this website, clients have the ability to buy and sell various assets in the Brazilian financial market, including stocks, options, derivatives, government bonds, and more. To make this possible, we utilized a variety of technologies such as HTML, CSS, JavaScript, C#, and Hibernate, in addition to integrations with other technologies within the bank.</p>
-                            <p>If you'd like to take a look, please visit:</p>
-                            <p>https://www.itaucorretora.com.br/.</p>
+                            <p className="p-project">In this project, I had the privilege of working both as a member of the development team and as a Scrum Master for a period of 5 months. It's worth noting that this website belongs to the largest bank in Latin America, Itaú.</p>
+                            <p className="p-project">On this website, clients have the ability to buy and sell various assets in the Brazilian financial market, including stocks, options, derivatives, government bonds, and more. To make this possible, we utilized a variety of technologies such as HTML, CSS, JavaScript, C#, and Hibernate, in addition to integrations with other technologies within the bank.</p>
+                            <p className="p-project">If you'd like to take a look, please visit:</p>
+                            <p className="p-project">https://www.itaucorretora.com.br/.</p>
                         </div>
                     </div>
                 </div>
